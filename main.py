bd16da9e8e97d2ea1763e8d6b381b1a6c982148e
JustYato <100683235+JustYato@users.noreply.github.com>
2022-10-12 22:40:35 +0300

    The beginning of writing the program

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+class Game(object):
+
+    letters = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")
+    ships_rules = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
+    field_size = len(letters)
+
+    def __init__(self):
+
+        self.players = []
+        self.current_player = None
+        self.next_player = None
+        self.status = 'prepare'
 
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def start_game(self)
+def status_check(self)
+def add_player(self, player)
+def ships_setup(self, player)
+def draw(self)
+def switch_players(self)
+def clear_screen()
